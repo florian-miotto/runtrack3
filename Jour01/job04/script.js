c8570be31c1bacb0a18166be14ceabd71a1006c3
florian-miotto <fmiotto@gmail.com>
2023-01-18 10:24:29 +0100

    done

---
+++ Jour01/job04/script.js
@@ -0,0 +1,23 @@
+// Maintenant que vous savez comment inclure du javascript et que vous maitrisez la
+// console web, vous allez pouvoir explorer davantage la syntaxe, la grammaire et le
+// lexique du langage javascript.
+// Pour l’ensemble des exercices suivants, vous devez rendre un fichier script.js contenant
+// le rendu de l’exercice et un fichier index.php qui l’inclut.
+// Déclarez une fonction “bisextile” qui prend en paramètre une variable “année”. Si l’année
+// est bisextile, la fonction retourne true, sinon elle retourne false.
+function bisextile(annee) {
+    if (annee % 4 === 0 && annee % 100!== 0 || annee % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+
+
+
+console.log(bisextile(2023)
+ );
+
---
+++ Jour01/job05/script.js
@@ -0,0 +1,13 @@
+// job 05
+// Créez une fonction “afficherjourssemaines”. Cette fonction ne prend pas de paramètre.
+// Créez un tableau de strings “jourssemaines” qui contient l’ensemble des jours de la
+// semaine, du Lundi au Dimanche. Ensuite à l’aide d’une boucle for (for!)Affichez un par
+// un ces jours.
+
+function afficherjourssemaines() {
+    var jourssemaines = ["Lundi", "Dimanche", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+    for (var i = 0; i < jourssemaines.length; i++) {
+        console.log(jourssemaines[i]);
+    }
+}
+
